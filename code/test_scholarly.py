d2707a546673c93f18419b4bb01535e4e1e09640
ma_paz <maria.poblete@sansano.usm.cl>
2024-01-17 17:25:50 -0300

    more

---
+++ code/test_scholarly.py
@@ -0,0 +1,19 @@
+import json
+import pandas as pd
+from scholarly import scholarly
+
+# Initialize an empty list to store the data
+author_data = []
+
+# will paginate to the next page by default
+authors = scholarly.search_keyword("mobile battery")
+
+for author in authors:
+    # Append each author's information to the list
+    author_data.append(author)
+
+# Create a DataFrame from the list of author data
+df = pd.DataFrame(author_data)
+
+# Save the DataFrame to an Excel file
+df.to_excel("mobile battery_authors.xlsx", index=False)
